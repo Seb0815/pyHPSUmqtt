4e1824f04b4dbd6cf206697fd792b0edbb6fa43a
N3rdix <tobias.abb+github@gmail.com>
2022-12-13 15:23:02 +0100

    Remove systemd, add MQTT to config

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ CONF_DIR="/etc/pyHPSU"
 BIN_DIR="/usr/bin/"
 PACKAGE_DIR="/usr/share/doc/packages/pyHPSU"
 DIST_DIR="/usr/lib/python3/dist-packages/HPSU"
-SERVICE_DIR="/etc/systemd/system/"
 echo "Installing pyHPSU"
 if [ ! -d $CONF_DIR ]; then
         echo "Creating directory for config files"
@@ -44,10 +43,6 @@ cp -r resources/* $SHARE_DIR
 cp -r HPSU/* $DIST_DIR
 cp -r contrib $DIST_DIR
 
-# copy service file
-cp systemd/* $SERVICE_DIR
-systemctl --system daemon-reload
-
 # copy binarys
 cp pyHPSU.py $BIN_DIR
 cp pyHPSUd.py $BIN_DIR
@@ -55,5 +50,3 @@ chmod a+x $BIN_DIR/pyHPSU.py
 chmod a+x $BIN_DIR/pyHPSUd.py
 
 echo "Installation done!!!"
-
-	
